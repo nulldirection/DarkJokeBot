53af4f216f9c0c996f68750c77d11d0895a22556
nulldirection <nikita-nino@mail.ru>
2021-10-02 02:37:00 +0300

    comment

---
+++ bot.py
@@ -0,0 +1,40 @@
+import os
+import random
+
+import telebot
+from telebot.types import Audio
+
+TOKEN = "1"
+bot = telebot.TeleBot(TOKEN)
+
+keyboard = telebot.types.ReplyKeyboardMarkup(True)
+keyboard.row('/start','Отправь шутку')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, 'Привет, чтобы получить шутку, напиши "Отправь шутку"', reply_markup=keyboard)
+"""
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+"""
+
+@bot.message_handler(content_types = ['text'])
+def send_message(message):
+	text = message.text.lower()
+	if text == 'отправь шутку':
+		bot.send_voice(message.chat.id, open(getRandomFile('jokes/'), 'rb'))
+
+def getRandomFile(path):
+  """
+  Returns a random filename, chosen among the files of the given path.
+  """
+  files = os.listdir(path)
+  index = random.randrange(0, len(files))
+  print(files[index])
+  return 'jokes/' + files[index]
+
+
+
+bot.polling()
